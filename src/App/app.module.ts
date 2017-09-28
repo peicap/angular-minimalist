951eea32825e0b6370de6a90acd9919fb9c3c947
peicap <peiwebdev@gmail.com>
2017-09-28 17:26:59 +0800

    import all required modules to run angular app

--- src/App/app.module.ts
+++ src/App/app.module.ts
@@ -3,10 +3,10 @@
  */
 
 import { NgModule } from '@angular/core';
-import { BrowserModule } from "@angular/platform-browser";
-import { Router } from '@angular/router'
-import { APP_ROUTING } from './app.routing'
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { APP_ROUTING } from './app.routing';
 /**
  * Import components below after manualy creating
  */
