11f982a9ea18777870c1bd9511150be958e790e4
peicap <peiwebdev@gmail.com>
2017-09-28 15:13:55 +0800

    Required vendors for Angular.

---
+++ src/vendors.ts
@@ -0,0 +1,15 @@
+/**
+ * Vendors is a way to specify all the dependecies 
+ * required by the project.
+ */
+
+// Angular 2
+import "@angular/platform-browser";
+import "@angular/platform-browser-dynamic";
+import "@angular/core";
+import "@angular/common";
+import "@angular/http";
+import "@angular/router";
+
+// RxJS
+import "rxjs";
