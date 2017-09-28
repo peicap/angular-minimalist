616b19b5ff09da8f5a62e07d2f0619aa368f0417
peicap <peiwebdev@gmail.com>
2017-09-28 15:13:32 +0800

    Polyfills for browsers stated by Angular.

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+/**
+ * A polyfill or polifiller is a piece of code that implements features 
+ * that browsers do not support. 
+*/
+
+import "core-js/es6";
+import "core-js/es7/reflect";
+require("zone.js/dist/zone");
